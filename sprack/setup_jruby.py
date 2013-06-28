09938c415c8b4567a19b1d71c6bf8c2a1ee213e3
Scott Clasen <scott@heroku.com>
2013-06-28 15:51:46 -0700

    fix start

--- sprack/setup_jruby.py
+++ sprack/setup_jruby.py
@@ -7,7 +7,7 @@ def start(args):
 
   try:
     subprocess.check_call("rvm jruby-1.7.4 do bundle install --gemfile=Gemfile", shell=True, cwd="sprack")
-    subprocess.check_call("rvm jruby-1.7.4 do ./start", shell=True)
+    subprocess.check_call("rvm jruby-1.7.4 do ./start", shell=True, cwd="sprack")
     return 0
   except subprocess.CalledProcessError:
     return 1
