2e49373e87e3c5a38a672a8b21a4788c293a5d25
Scott Clasen <scott@heroku.com>
2013-06-28 22:57:25 -0700

    processes

--- sprack/config.ru
+++ sprack/config.ru
@@ -1,3 +1,4 @@
 require './hello_world'
 
-run Sinatra::Application
+#run Sinatra::Application
+run lambda { |env| [200, {"Content-Type" => "application/json"}, ['{"message":"Hello World!"}']] }
---
+++ sprack/setup.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+import time
+import os
+
+def start(args=None):
+    subprocess.check_call("rvm jruby-1.7.4 do bundle install", shell=True, cwd="sprak")
+    subprocess.Popen("rvm jruby-1.7.4 do ./start", cwd="sprack", shell=True)
+    time.sleep(5u
+    return 0
+
+def stop():
+  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+  for line in out.splitlines():
+    if 'sprack' in line:
+      try:
+        pid = int(line.split(None, 2)[1])
+        os.kill(pid, 9)
+      except OSError:
+        pass
+
+  return 0
